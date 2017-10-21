c7c196ac2f9fc5326cf613bfb0fa8e50c957248f
Xianling(Lily) Zhang <alchemxz@gmail.com>
2017-10-21 18:11:29 -0400

    Incomplete

---
+++ Ongoing/Hello_Cat/Assets/Scripts/CatToMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CatToMove : MonoBehaviour {
+    public Transform startMarker;
+    public Vector3 endMarker;
+    private float speed = 0.05F;
+    private float startTime;
+    private float journeyLength;
+    public GameObject textMove;
+
+    void Start()
+    {
+        //initially, the object will not move when the journeyLength is 0
+        journeyLength = 0;   
+    }
+
+    void Update()
+    {
+        textMove.GetComponent<TextMesh>().text = "pos:" + this.transform.position;
+
+       
+        if (journeyLength > 0)
+        {
+            //distance 
+            float distCovered = (Time.time - startTime) * speed;
+            //percentage of distance moved
+            float fracJourney = distCovered / journeyLength;
+            transform.position = Vector3.Lerp(startMarker.position, endMarker, fracJourney);
+            
+        }
+    }
+
+    //send the endPos from HelloAR script when "Hit Made" to know where to go
+    public void StartMove(Vector3 endPos)
+    {
+        startMarker = this.transform;
+        endMarker = endPos;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startMarker.position, endMarker);
+        Debug.Log("journeyLength is " + journeyLength);
+    }
+}
---
+++ Ongoing/Hello_Cat/Assets/Scripts/PositionTracking.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionTracking : MonoBehaviour {
+
+    public GameObject TextObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TextObject.GetComponent<TextMesh>().text = "Pos: " + this.transform.position;
+	}
+}
